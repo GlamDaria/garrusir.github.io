467a4827866000565441e015f672a4f4671cd5cb
Daria <daryalutkova@yandex.ru>
2019-09-19 20:14:37 +0300

    fourteenth glam-commit with fetch and final forms

--- main.js
+++ main.js
@@ -85,6 +85,11 @@ function callMeBack(){
 	let btn = document.getElementById('callMeBack');
 	let hid = document.getElementById("hiddenDiv");
 	let close = document.getElementById("closePopUp");
+	hid.onclick = () =>{
+		if (event.target===event.currentTarget){
+			hid.style.display = "none";
+		}
+	}
 	btn.onclick = function(){
 		hid.style.display = "block";
 	}
@@ -93,6 +98,75 @@ function callMeBack(){
 	}
 }
 
+function clickSend(){
+	let form = document.getElementById('formPopUp');
+	form.addEventListener('submit', (event) =>{
+		event.preventDefault();
+		let send_name = form.querySelector('input[name="name"]');
+		let send_phone = form.querySelector('input[name="phone"]');
+		sendForm(send_name.value, send_phone.value, resolvedSend, rejectedSend);
+	})
+	let form2 = document.querySelector('.form-field>form');
+	form2.addEventListener('submit', (event) =>{
+		event.preventDefault();
+		let send_phone = form2.querySelector('#phone1');
+		let send_name = form2.querySelector('input[name="name"]');
+		sendForm(send_name.value, send_phone.value, ()=>{
+			document.querySelector('.card').classList.add('active_flip');
+		}, () =>{
+			document.getElementById('error_send').innerHTML = "Ошибка отправки";
+		});
+	})
+}
+function sendForm(name, phone, resolved, rejected){
+	let body = new FormData;
+	body.append("name", name);
+	body.append('phone', phone);
+	fetch('http://localhost:8080/gay.php', {
+		'method' : "POST",
+		'body': body,
+		"headers" : new Headers()
+	})
+		.then(response => {
+			if(response.status >= 200 && response.status < 300)
+				return response.text();
+		})
+		.then(text => resolved())
+		.catch(error => {
+			rejected();
+			console.log(error);
+		})
+}
+function rejectedSend(){
+	let d = document.createElement('div');
+	d.innerHTML = `<svg id="closePopUp" version="1.1" x="0px" y="0px" viewBox="0 0 469.785 469.785" xml:space="preserve"><g transform="matrix(1.25 0 0 -1.25 0 45)"><g><g>
+			<path style="fill:#000000;" d="M228.294-151.753L367.489-12.558c11.116,11.105,11.116,29.116,0,40.22
+				c-11.105,11.116-29.104,11.116-40.22,0L188.073-111.533L48.866,27.663c-11.093,11.116-29.116,11.116-40.22,0
+				c-11.105-11.105-11.105-29.116,0-40.22l139.207-139.196L8.338-291.268c-11.116-11.116-11.116-29.116,0-40.22
+				c5.552-5.564,12.834-8.34,20.116-8.34c7.27,0,14.552,2.776,20.105,8.34l139.514,139.514l139.196-139.196
+				c5.564-5.552,12.834-8.34,20.116-8.34c7.27,0,14.552,2.788,20.105,8.34c11.116,11.105,11.116,29.104,0,40.22L228.294-151.753z"/></g></g></g></svg><span>Ой, что-то пошло не так</span><p>К сожалению, форма не была отправлена.<br> Позвоните нам по номеру: +7 925 076 97 69.</p>`;
+	d.id = "sended";
+	d.querySelector('#closePopUp').onclick = () =>{
+		document.getElementById("hiddenDiv").style.display = "none";
+	}
+	document.getElementById('formPopUp').innerHTML = "";
+	document.getElementById('formPopUp').append(d);
+}
+function resolvedSend(){
+	let d = document.createElement('div');
+	d.innerHTML = `<svg id="closePopUp" version="1.1" x="0px" y="0px" viewBox="0 0 469.785 469.785" xml:space="preserve"><g transform="matrix(1.25 0 0 -1.25 0 45)"><g><g>
+			<path style="fill:#000000;" d="M228.294-151.753L367.489-12.558c11.116,11.105,11.116,29.116,0,40.22
+				c-11.105,11.116-29.104,11.116-40.22,0L188.073-111.533L48.866,27.663c-11.093,11.116-29.116,11.116-40.22,0
+				c-11.105-11.105-11.105-29.116,0-40.22l139.207-139.196L8.338-291.268c-11.116-11.116-11.116-29.116,0-40.22
+				c5.552-5.564,12.834-8.34,20.116-8.34c7.27,0,14.552,2.776,20.105,8.34l139.514,139.514l139.196-139.196
+				c5.564-5.552,12.834-8.34,20.116-8.34c7.27,0,14.552,2.788,20.105,8.34c11.116,11.105,11.116,29.104,0,40.22L228.294-151.753z"/></g></g></g></svg><span>Спасибо за заявку!</span><p>Мы с Вами свяжемся в ближайшее время</p>`;
+	d.id = "sended";
+	d.querySelector('#closePopUp').onclick = () =>{
+		document.getElementById("hiddenDiv").style.display = "none";
+	}
+	document.getElementById('formPopUp').innerHTML = "";
+	document.getElementById('formPopUp').append(d);	
+}
 function masking(){
 	document.getElementById('phone').addEventListener('input', function (e) {
   	var x = e.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
@@ -178,6 +252,7 @@ window.onload = () => {
 	slider();
 
 	callMeBack();
+	clickSend();
 	masking();
 	fixNav();
 
